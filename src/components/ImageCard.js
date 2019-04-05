cad1e7f6ae61d01c4ba7b6441e5ea946b06ce964
Steve Ferenchak <steveferenchak@gmail.com>
2019-04-05 15:14:11 -0700

    102.) Create ImageCard Component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ImageCard extends React.Component {
+	render() {
+		const { alt_description, urls } = this.props.image;
+		return (
+			<div>
+				<img src={urls.regular} alt={alt_description} />
+			</div>
+		);
+	}
+}
